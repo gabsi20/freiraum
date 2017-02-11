bcbee3568aa29e9a66efb9605abbea7e81799ca2
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-02-11 09:36:10 +0100

    add: custom_role for cms user

--- public/wp-content/themes/freiraum-abtenau.at/functions.php
+++ public/wp-content/themes/freiraum-abtenau.at/functions.php
@@ -5,6 +5,24 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'gallery_image', 1024, 768, false );
 }
 
+add_role( 'custom_role', 'Freiraum Editor', array());
+
+function add_theme_caps() {
+    // gets the author role
+	$role = get_role( 'custom_role' );
+
+    // This only works, because it accesses the class instance.
+    // would allow the author to edit others' posts for current theme only
+	$role->add_cap( 'read' );  
+	$role->add_cap( 'level_0' );  
+	$role->add_cap( 'edit_pages' ); 
+	$role->add_cap( 'edit_others_pages' ); 
+	$role->add_cap( 'edit_published_pages' ); 
+	// $role->remove_cap('edit_posts');
+	$role->add_cap( 'upload_files' ); 
+}
+add_action( 'admin_init', 'add_theme_caps');
+
 /*function sortFunction( $a, $b ) {
 	return strtotime($a["date"]) - strtotime($b["date"]);
 }
