59e3bd5379fd56a4a80cb159fbd414098aff1fa0
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-02-11 11:03:15 +0100

    update: seo

--- public/wp-content/themes/freiraum-abtenau.at/inc/header.php
+++ public/wp-content/themes/freiraum-abtenau.at/inc/header.php
@@ -3,11 +3,13 @@
 <head> 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>freiRaum - Abtenau</title>
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#7F7359">
+    <meta name="theme-color" content="#82bf5b">
+    <meta name="title" content="freiRaum - Abtenau">
+    <meta name="description" content="Du suchst einen Ort, wo Du Deine Dienste, Kurse und Trainings durchführen möchtest? Wir stellen Dir die Infrastruktur und das Ambiente zur Verfügung.">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.min.css">
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png">
     <!--[if lt IE 9]>
