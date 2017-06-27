47b90966a91763cdfd2d9304da48fe90315ffa84
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2017-06-27 11:58:07 +0200

    feat: clickable image in events

--- public/wp-content/themes/freiraum-abtenau.at/modules/events.php
+++ public/wp-content/themes/freiraum-abtenau.at/modules/events.php
@@ -2,15 +2,20 @@
 
 <div class="calendar">
 	<ul class="calendar__list">
-		<?php foreach ($events as $i => $event) { ?>
-		<li class="calendar__item">
-			<div class="calendar__head js-calendar-head">
+		<?php foreach ($events as $i => $event) { 
+			$image = $event['image'];
+			?>
+			<li class="calendar__item">
+				<div class="calendar__head js-calendar-head">
 
-				<div class="calendar__date"><?= $event['date'] ?></div>
-				<div class="calendar__title"><?= $event['title'] ?></div>
-			</div>
-			<div class="calendar__description"><?= $event['description'] ?></div>
-		</li>
-		<?php } ?>
-	</ul>
-</div>
+					<div class="calendar__date"><?= $event['date'] ?></div>
+					<div class="calendar__title"><?= $event['title'] ?></div>
+				</div>
+				<div class="calendar__description">
+					<?= $event['description'] ?>
+					<div class="calendar__image js-gallery-item" style="background-image: url('<?= $image['sizes']['gallery_image'] ?>')" data-image="<?= $image['sizes']['gallery_image'] ?>"></div>
+				</div>
+			</li>
+			<?php } ?>
+		</ul>
+	</div>
